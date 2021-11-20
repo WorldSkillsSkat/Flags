aae1e2274c008fdcf3e17c405b6ce30b18473215
1_skat_1 <skat.apps@gmail.com>
2021-11-20 14:29:35 +0300

    realization register

--- app/src/main/java/com/example/task1/authorization/register/RegisterFragment.kt
+++ app/src/main/java/com/example/task1/authorization/register/RegisterFragment.kt
@@ -5,19 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.task1.R
+import com.example.task1.databinding.RegisterFragmentBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class RegisterFragment : Fragment() {
 
 
     private val viewModel: RegisterViewModel by viewModel<RegisterViewModel>()
+    private lateinit var binding: RegisterFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.register_fragment, container, false)
+        initBinding(inflater, container)
+        setupObserverViewModel()
+        return binding.root
+    }
+
+    private fun initBinding(inflater: LayoutInflater, container: ViewGroup?) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.register_fragment, container, false)
+        binding.email = ""
+        binding.password = ""
+        binding.firstName = ""
+        binding.lastName = ""
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    private fun setupObserverViewModel() {
+        viewModel.openFragsFragment.observe(viewLifecycleOwner, {
+            if (it) findNavController().navigate(R.id.action_registerFragment_to_flagsFragment)
+        })
+
+        viewModel.showToast.observe(viewLifecycleOwner, {
+            showToast(it)
+        })
+    }
+
+    private fun showToast(msg: String) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/example/task1/authorization/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/task1/authorization/register/RegisterViewModel.kt
@@ -1,8 +1,49 @@
 package com.example.task1.authorization.register
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.task1.data.NetworkService
+import com.example.task1.data.User
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
-class RegisterViewModel(networkService: NetworkService) : ViewModel() {
+class RegisterViewModel(private val networkService: NetworkService) : ViewModel() {
+    private var _showToast = MutableLiveData<String>()
+    val showToast: LiveData<String>
+        get() = _showToast
+
+    private var _openFlagsFragment = MutableLiveData<Boolean>(false)
+    val openFragsFragment: LiveData<Boolean>
+        get() = _openFlagsFragment
+
+    private val emailPattern = Regex("\\w+@[a-z\\d]+[.][a-z]{1,3}")
+
+    fun register(firstName: String, lastName: String, email: String, password: String) {
+
+        if (email != "" && password != "" && firstName != "" && lastName != "") {
+            if (emailPattern.matches(email)) {
+                addUser(firstName, lastName, email, password)
+            } else {
+                _showToast.value = "Ваша почта не валидна. Попробуйте еще раз"
+            }
+        } else {
+            _showToast.value = "Не все поля заполнены"
+        }
+    }
+
+    fun addUser(firstName: String, lastName: String, email: String, password: String) {
+        viewModelScope.launch {
+            try {
+                networkService.addUser(User(firstName, lastName, email, password))
+                _openFlagsFragment.value = true
+            } catch (ex: HttpException) {
+                _showToast.value = "Ошибка на сервере. Попробуйте еще раз"
+            }
+        }
+
+        }
+
 
 }
