822d953b4878aa0adb2d79f47a71ad24660cb6f1
1_skat_1 <skat.apps@gmail.com>
2021-11-20 19:08:42 +0300

    realization flagsViewModel

--- app/src/main/java/com/example/task1/country/FlagsViewModel.kt
+++ app/src/main/java/com/example/task1/country/FlagsViewModel.kt
@@ -1,15 +1,34 @@
 package com.example.task1.country
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.task1.data.NetworkService
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
-class FlagsViewModel(networkService: NetworkService) : ViewModel() {
+class FlagsViewModel(private val networkService: NetworkService) : ViewModel() {
     init {
-
+        getCountries()
     }
 
-    private fun getCountries() {
+    private var _countries = MutableLiveData<Array<Country>>()
+    val countries: LiveData<Array<Country>>
+        get() = _countries
 
+    private var _showToast = MutableLiveData<String>()
+    val showToast: LiveData<String>
+        get() = _showToast
+
+    private fun getCountries() {
+        viewModelScope.launch {
+            try {
+                _countries.value = networkService.getCountries()
+            } catch (ex: HttpException) {
+                _showToast.value = "Ошибка сервера"
+            }
+        }
     }
 
 }
