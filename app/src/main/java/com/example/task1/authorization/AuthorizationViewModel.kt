e5aa2e634ad00c51046e50077b24dd3101499fa9
1_skat_1 <skat.apps@gmail.com>
2021-11-20 10:39:09 +0300

    realization logic signIn

--- app/src/main/java/com/example/task1/authorization/AuthorizationViewModel.kt
+++ app/src/main/java/com/example/task1/authorization/AuthorizationViewModel.kt
@@ -6,9 +6,27 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.task1.data.NetworkService
+import com.example.task1.data.User
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 
 class AuthorizationViewModel(private val networkService: NetworkService) : ViewModel() {
+    private var _openFlagsFragment = MutableLiveData<Boolean>(false)
+    val openFragsFragment: LiveData<Boolean>
+        get() = _openFlagsFragment
 
+    private var _isFailure = MutableLiveData<Boolean>(false)
+    val isFailure: LiveData<Boolean>
+        get() = _isFailure
+
+    fun signIn(email: String, password: String) {
+        viewModelScope.launch {
+            try {
+                networkService.signIn(User(email, password))
+                _openFlagsFragment.value = true
+            } catch (ex: HttpException) {
+                _isFailure.value = true
+            }
+        }
+    }
 }
